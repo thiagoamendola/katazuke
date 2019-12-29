6cb9a0601086c3dcec16f976192e950ea5d7d32d
Thiago Amendola <amendolathiago@gmail.com>
2019-12-29 20:18:39 -0300

    Create tutorial screen and first animation

---
+++ Assets/Scripts/TutorialAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialAnimator : MonoBehaviour
+{
+    
+    string lastPlayerAnim;
+
+    public void TriggerPlayerAnimation(string animName){
+        Animator playerAnimator = transform.Find("Player1").Find("Character").gameObject.GetComponent<Animator>();
+        if (lastPlayerAnim != "")
+            playerAnimator.SetBool(lastPlayerAnim, false);
+        switch(animName){
+            case "hold":
+                playerAnimator.ResetTrigger(animName);
+                playerAnimator.SetTrigger(animName);
+                lastPlayerAnim = "";
+                break;
+            default:
+                playerAnimator.SetBool(animName, true);
+                lastPlayerAnim = animName;
+                break;
+        }
+    }
+
+
+
+
+}
